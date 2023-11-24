ec15baba87682b3a77d73e18b75e1ee4f14fe0e2
hobospider132 <christian.duong44@gmail.com>
2023-11-24 18:35:03 +1100

    oragnisation

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func new_game():
+	Player.start($StartPosition.position)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
