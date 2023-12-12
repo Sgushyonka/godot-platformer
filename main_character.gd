901410995a54786994e60a0db887ba16d7ad583c
Hobospider132 <100825450+Hobospider132@users.noreply.github.com>
2023-12-12 17:30:56 +1100

    Update main_character.gd

--- main_character.gd
+++ main_character.gd
@@ -1,25 +1,17 @@
 extends CharacterBody2D
 
-@export var gravity = 100
-@export var jump_velocity = 400
+@export var gravity = 18
+@export var jump_velocity = 11
 @export var speed = 120
 @export var crouch_speed = 80
 @export var sprint_speed = 180
-
-var start_y
-
-func _ready():
-	start_y = position.y 
+@export var min_y = -530
+@export var max_y = -2
+var start_y = -2
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.x = 0
-		velocity.y = 0 
-		while position.y != start_y:
-			velocity.y += gravity * delta
-
-	
+	# Add the gravity		
+	velocity.y += gravity * delta
 	
 	if Input.is_action_pressed("ForwardD") or Input.is_action_pressed("ForwardArrow"):
 		if Input.is_action_pressed("Crouch"):
@@ -36,29 +28,36 @@ func _physics_process(delta):
 			velocity.x = -sprint_speed
 		else:
 			velocity.x = -speed
-	
 	else:
 		velocity.x = 0
 	
-		# Handle Jump.
-	if Input.is_action_just_pressed("JumpW") and is_on_floor() or Input.is_action_just_pressed("JumpArrow") and is_on_floor():
-		velocity.y = -jump_velocity
-		velocity.x = speed
+		# Handle Jump, idk why is_on_floor() is needed but code breaks without, it looks useless but leave it.
+		# It must also be not is_on_floor()
 
+	if Input.is_action_pressed("JumpW") and not is_on_floor() or Input.is_action_pressed("JumpArrow") and not is_on_floor():
+		velocity.y -= jump_velocity
+	
+	if position.y >= -120:
+		velocity.y += gravity * delta
 
 	if velocity.length() > 0:
-		if not Input.is_action_pressed("Crouch") and Input.is_action_pressed("Sprint"):
+		if Input.is_action_pressed("ForwardArrow") or Input.is_action_pressed("ForwardD") and not Input.is_action_pressed("Crouch") and Input.is_action_pressed("Sprint"):
 			velocity = velocity.normalized() * sprint_speed
 			$movement.animation = "run"
 			$movement.play()
-		elif Input.is_action_pressed("Crouch"):
+		elif Input.is_action_pressed("ForwardArrow") or Input.is_action_pressed("ForwardD") and Input.is_action_pressed("Crouch"):
 			velocity = velocity.normalized() * crouch_speed
 			$movement.animation = "crouch"
 			$movement.play()
-		else:
+		elif Input.is_action_pressed("JumpW") or Input.is_action_pressed("JumpArrow"):
+			$movement.animation = "jump"
+			$movement.play()
+		elif Input.is_action_pressed("ForwardD") or Input.is_action_pressed("BackwardA") or Input.is_action_pressed("ForwardArrow") or Input.is_action_pressed("BackwardArrow"):
 			velocity = velocity.normalized() * speed
 			$movement.animation = "walk-good"
 			$movement.play()
+		else:
+			$movement.stop()
 	else: 
 		# Reset to default animation
 		$movement.animation = "walk-good"
@@ -70,8 +69,8 @@ func _physics_process(delta):
 	if velocity.x > 0:
 		$movement.flip_h = false
 		
-		move_and_slide()
-		
 	position += velocity * delta
+	
+	position.y = max(min_y, min(max_y, position.y))
 
 	
